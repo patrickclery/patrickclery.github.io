d67da8528b4758de95e927c116394963c7a8604f
patrickclery <patrick.clery@gmail.com>
2020-04-19 03:57:04 -0700

    Updates

---
+++ precache-manifest.dc358231c70c6d8db5316cc8d3ae512f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98be5ce8685597f7e47a8adccc9d414",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c423530ad2924761e1a2",
+    "url": "/static/css/main.cccec728.chunk.css"
+  },
+  {
+    "revision": "c423530ad2924761e1a2",
+    "url": "/static/js/main.ef1d3d20.chunk.js"
+  },
+  {
+    "revision": "c262cfef19c2bbc706a6",
+    "url": "/static/js/runtime-main.27ab6645.js"
+  },
+  {
+    "revision": "d06f40dddea1d7c7576883ecd75819c5",
+    "url": "/static/media/bio-photo.d06f40dd.jpg"
+  }
+]);
